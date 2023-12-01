778b2daa6a53aa9025b72257122fbe2710fb4708
giuliavasconcelos <giuliavasconcelos.sesisenai@gmail.com>
2023-12-01 15:33:35 -0300

    css

--- templates/footer.php
+++ templates/footer.php
@@ -1,2 +1,48 @@
+<?php
+    include_once()
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+
+    <!-- LINK CSS -->
+    <link rel="stylesheet" href="../css/footer.css">
+
+    <!-- LINK BOOTSTRAP -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+    
+    <footer>
+        <div class="footer-content">
+            <img src="../assets/menzy_logo_footer.png" alt="logo">
+            <p>Sempre conectando você aos melhores eventos.</p>
+            <ul class="socials">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <p>copyright &copy; <a href="#">Menzy</a>  </p>
+                    <div class="footer-menu">
+                      <ul class="f-menu">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Ticket</a></li>
+                        <li><a href="">Contato</a></li>
+                        <li><a href="">Perfil</a></li>
+                      </ul>
+                    </div>
+        </div>
+
+    </footer>
+
 </body>
 </html>
