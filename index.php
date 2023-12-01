b1dbbf2ebb9ecde118314a368c734c5228a8ad32
anacasagrandi <ana.aninha.beatriz.06@gmail.com>
2023-12-01 11:12:54 -0300

    a

--- index.php
+++ index.php
@@ -64,13 +64,13 @@
   <!-- <div id="carousel" class="carousel slide" data-bs-ride="carousel"> -->
   <div id="carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
     <div class="carousel-inner">
-      <div class="carousel-item active" data-bs-interval="3000">
-        <img src="assets/slides/bence.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-item active" data-bs-interval="2000">
+        <img src="assets/slides/arte_pqno.jpg" class="d-block w-100" alt="...">
       </div>
-      <div class="carousel-item" data-bs-interval="3000">
-        <img src="assets/slides/arturo.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-item" data-bs-interval="2000">
+        <img src="assets/slides/abby.jpg" class="d-block w-100" alt="...">
       </div>
-      <div class="carousel-item" data-bs-interval="3000">
+      <div class="carousel-item" data-bs-interval="2000">
         <img src="assets/slides/ella.jpg" class="d-block w-100" alt="...">
       </div>
     </div>
